d4e28eeaa340b33ab7672a1bf15fd329ad365061
Kavindu Fernando <kavindu2331@gmail.com>
2025-02-05 23:12:58 +0530

    feature : setup express server

--- chapter_2/server.js
+++ chapter_2/server.js
@@ -6,6 +6,14 @@ const PORT = 3000;
 console.log("This is ane xtra file")
 
 
-app.listen(PORT, () => {
-    console.log('Server is running on port:', PORT);
-    });
+
+//HTTP Verbs
+
+app.get('/', (req, res) => {
+    res.sendStatus(200);
+
+});
+
+
+
+app.listen(PORT, () => {console.log('Server is running on port:', PORT); });
