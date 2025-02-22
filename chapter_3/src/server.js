7f2ff0ec0e6fe05ca0cc163c09dfaf54b343f999
Kavindu Fernando <kavindu2331@gmail.com>
2025-02-22 22:40:20 +0530

    feat: server changes

--- chapter_3/src/server.js
+++ chapter_3/src/server.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+// Get the File path
+
+const __filename = fileURLToPath(import.meta.url);
+
+// Get the directory path
+const __dirname = dirname(__filename);
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path)
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
